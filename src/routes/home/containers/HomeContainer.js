9361f2b696bd45154d0cea4cc4513587d2fb337e
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-20 16:01:27 -0700

    Remove all files to start from scratch for V2

--- src/routes/home/containers/HomeContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-
-import HomeView from '../components/HomeView';
-
-const mapDispatchToProps = {};
-const mapStateToProps = () => ({}); // state => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
--- src/routes/my_route/constants/index.js
+++
@@ -1,1 +0,0 @@
-export const MY_ROUTE_ACION = 'MY_ROUTE_ACION';
