d6c21908169ee52b83fde7ab4c3557dd91d445f7
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-20 17:05:44 -0700

    Init npm and add minimal webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 configs = {
-
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js',
+    publicPath: 'http://localhost:8080/'
+  }
 };
 
 module.exports = configs;
