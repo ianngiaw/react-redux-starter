db8b0502b5b4e12ae2c80788bbae0fb96ae5c81a
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-27 14:29:24 -0700

    Add loaders and preloaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,53 @@
-configs = {
-  entry: __dirname + '/src/app.js',
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const configs = {
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:8080',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, '/src/app.js')
+  ],
   output: {
-    path: __dirname + '/dist',
+    path: path.join(__dirname, '/dist'),
     filename: 'app.bundle.js',
     publicPath: 'http://localhost:8080/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.template.ejs',
+      inject: 'body'
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/,
+        loader: 'eslint?parser=babel-eslint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.scss$/,
+        loader: "style!css?modules!sass"
+      }
+    ]
   }
 };
 
